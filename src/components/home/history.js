e0e0cc56f9e154ec2e40ce04e4049e3066af10f5
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-07-28 10:31:21 -0700

    Refactoring : remove movies Tag

--- src/components/home/history.js
+++ src/components/home/history.js
@@ -13,29 +13,14 @@ async function viewsMovies() {
     }
 }
 
-async function tagMovies() {
-    try {
-      const response = await axios.get('http://127.0.0.1:5000/movie/history/tag/email/' + localStorage.getItem('email'));
-      console.log(response);
-      return response;
-    } catch (error) {
-      console.error(error);
-    }
-}
-
 export default class History extends Component {
 
-state = { value: 2,moviesViews: [],moviesTag: [] };
+state = { value: 2,moviesViews: [] };
 
 componentDidMount =()=>{
 	viewsMovies().then(response => {
 	  this.setState({
 		moviesViews: response.data
-	  });
-	});
-	tagMovies().then(response => {
-	  this.setState({
-		moviesTag: response.data
 	  });
 	});
 }
@@ -62,46 +47,18 @@ const historyViews = this.state.moviesViews.map((element) =>
     </div>
 );
 
-
-const historyTag = this.state.moviesTag.map((element) => 
-	<div className="card mb-5 border-0 rounded-0 shadow-sm bg-light">
-	<div className="row no-gutters">
-		<div className="col-md-2">
-			<img src={element[13]} class="card-img rounded-0" alt="..." />
-		</div>
-		<div className="col-md-10">
-			<div className="card-body">
-				<h5 className="card-title">{element[3]}</h5>
-				<small className="text-muted">{element[4]}</small><br />
-				<small className="text-muted">{element[5]}</small><br />
-				<small className="text-muted">{element[6]}</small><br />
-				<small className="text-muted">{element[7]}</small><br />
-				<Link className="btn btn-primary btn-sm position-relative" to={"/movie/single/" + element[0]} style={{top: "35px"}}>Show more</Link>
-			</div>
-		</div>
-	</div>
-    </div>
-);
-	
-
-
 return (
 <div className="container mt-5">
 
 <nav className="mb-3">
   <div class="nav nav-tabs" id="nav-tab" role="tablist">
     <a class="nav-item nav-link active" id="nav-view-tab" data-toggle="tab" href="#nav-view" role="tab" aria-controls="nav-cviewontact" aria-selected="true">View</a>
-    <a class="nav-item nav-link text-muted" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Tag</a>
   </div>
 </nav>
 <div class="tab-content" id="nav-tabContent">
 
 <div class="tab-pane fade show active" id="nav-view" role="tabpanel" aria-labelledby="nav-view-tab">
 {historyViews}
-</div>
-
-<div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
-{historyTag}
 </div>
 
 </div>
